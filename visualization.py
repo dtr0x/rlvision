16d23f2c8a08fd817cae462d029cc3e49ae170c0
Dylan Troop <dylantroop@gmail.com>
2019-11-12 12:15:58 -0500

    visualization of confidence score for testing.

--- visualization.py
+++ visualization.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter
 from dataloader import state_transform
 from reinforcement import *
 import math
@@ -11,12 +11,18 @@ def adjust_bbox_for_draw(bbox):
 
 def draw_boxes(state):
     image = state.image.copy()
-    draw = ImageDraw.Draw(image)
     bbo = adjust_bbox_for_draw(state.bbox_observed)
     bbt = adjust_bbox_for_draw(state.bbox_true)
-    draw.rectangle(bbo, outline=(0,255,255))
-    draw.rectangle(bbt, outline=(255,0,255))
-    return(image)
+    ob_region = image.crop(bbo)
+    gt_region = image.crop(bbt)
+    blur_img = image.filter(ImageFilter.GaussianBlur(radius=10))
+    blur_gt = gt_region.filter(ImageFilter.GaussianBlur(radius=4))
+    blur_img.paste(blur_gt, bbt)
+    draw = ImageDraw.Draw(blur_img)
+    blur_img.paste(ob_region, bbo)
+    draw.rectangle(bbo, outline=(255,0,0))
+    draw.rectangle(bbt, outline=(0,0,0))
+    return(blur_img)
 
 def draw_boxes_conf(state, classifier):
     image = state.image.copy()
@@ -73,6 +79,28 @@ def draw_localization_actions(state, max_n_actions, net):
     if vis.width > MAX_IMAGE_WIDTH:
         vis = last_action_image
     return vis, action_sequence, iou
+
+def draw_localization_actions_gif(state, max_n_actions, net):
+    action_sequence = []
+    w = state.image.width
+    h = state.image.height
+    frames = []
+    frames.append(draw_boxes(state))
+    done = False
+    for i in range(max_n_actions):
+        img_t, action_history = state_transform([state])
+        action = net(img_t, action_history).max(1).indices[0].item()
+        action_sequence.append(action)
+        reward, state, done = take_action(state, action)
+        frame = draw_boxes(state)
+        if done:
+            draw = ImageDraw.Draw(frame)
+            draw.text((0,0), "{} actions taken.".format(len(frames)), (0,0,0))
+            frames.append(frame)
+            break
+        frames.append(frame)
+    iou = calculate_iou(state)
+    return frames, action_sequence, iou
 
 def draw_sequence_with_conf_score(state, max_n_actions, net, classifier):
     action_sequence = []
